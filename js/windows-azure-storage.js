e64ca498d65724ab09dce8ecb83bd6b63ffed1d2
Morgan Estes <morgan.estes@10up.com>
2016-01-10 22:36:42 -0600

    JSHint fixes: single quote strings in JS
    Azure plugin formatting changes to single-quote strings.

--- js/windows-azure-storage.js
+++ js/windows-azure-storage.js
@@ -1,57 +1,57 @@
 /* global windowsAzureStorageSettings:false */
 function onUpload_ContainerSelectionChanged() {
-	var htmlForm = document.getElementsByName( "UploadNewFileForm" )[0];
-	if ( htmlForm.elements["selected_container"].value === windowsAzureStorageSettings.l10n.newContainer ) {
-		htmlForm.elements["uploadFileTag"].disabled = true;
-		htmlForm.elements["uploadFileTag"].style.background = "gray";
-		htmlForm.elements["uploadFileName"].disabled = true;
-		htmlForm.elements["uploadFileName"].style.background = "gray";
-		htmlForm.elements["createContainer"].style.visibility = "visible";
-		document.getElementById( 'lblNewContainer' ).style.display = "block";
-		//document.getElementsByName("createContainer")[0].style.display = "block";
-		htmlForm.elements["action"].value = 'Create';
-		htmlForm.elements["submit"].value = 'Create';
+	var htmlForm = document.getElementsByName( 'UploadNewFileForm' )[0];
+	if ( htmlForm.elements['selected_container'].value === windowsAzureStorageSettings.l10n.newContainer ) {
+		htmlForm.elements['uploadFileTag'].disabled = true;
+		htmlForm.elements['uploadFileTag'].style.background = 'gray';
+		htmlForm.elements['uploadFileName'].disabled = true;
+		htmlForm.elements['uploadFileName'].style.background = 'gray';
+		htmlForm.elements['createContainer'].style.visibility = 'visible';
+		document.getElementById( 'lblNewContainer' ).style.display = 'block';
+		//document.getElementsByName('createContainer')[0].style.display = 'block';
+		htmlForm.elements['action'].value = 'Create';
+		htmlForm.elements['submit'].value = 'Create';
 	} else {
-		htmlForm.elements["uploadFileTag"].disabled = false;
-		htmlForm.elements["uploadFileTag"].style.background = "white";
-		htmlForm.elements["uploadFileName"].disabled = false;
-		htmlForm.elements["uploadFileName"].style.background = "white";
-		htmlForm.elements["createContainer"].style.visibility = "hidden";
-		document.getElementById( 'lblNewContainer' ).style.display = "none";
-		//document.getElementsByName("createContainer")[0].style.display = "none";
-		htmlForm.elements["action"].value = windowsAzureStorageSettings.l10n.upload;
-		htmlForm.elements["submit"].value = windowsAzureStorageSettings.l10n.upload;
+		htmlForm.elements['uploadFileTag'].disabled = false;
+		htmlForm.elements['uploadFileTag'].style.background = 'white';
+		htmlForm.elements['uploadFileName'].disabled = false;
+		htmlForm.elements['uploadFileName'].style.background = 'white';
+		htmlForm.elements['createContainer'].style.visibility = 'hidden';
+		document.getElementById( 'lblNewContainer' ).style.display = 'none';
+		//document.getElementsByName('createContainer')[0].style.display = 'none';
+		htmlForm.elements['action'].value = windowsAzureStorageSettings.l10n.upload;
+		htmlForm.elements['submit'].value = windowsAzureStorageSettings.l10n.upload;
 	}
 }
 
 function insertImageTag( imgURL, containsSignature ) {
 	var imageFullURL = imgURL;
-	var imageTag = "";
-	if ( containsSignature == "true" ) {
-		var st = imageFullURL.indexOf( "?st=" );
+	var imageTag = '';
+	if ( containsSignature == 'true' ) {
+		var st = imageFullURL.indexOf( '?st=' );
 		if ( st != - 1 ) {
 			imgURL = imageFullURL.substr( 0, st );
 		}
 	}
 
-	var dot = imgURL.lastIndexOf( "." );
+	var dot = imgURL.lastIndexOf( '.' );
 	if ( dot == - 1 ) {
-		imageTag = '<a href="' + imgURL + '" >' + imgURL + '</a> ';
+		imageTag = '<a href="' + imgURL + '">' + imgURL + '</a> ';
 	}
 	else {
 		var extension = imgURL.substr( dot, imgURL.length );
 		switch ( extension.toLowerCase() ) {
-			case ".jpg":
-			case ".jpeg":
-			case ".gif":
-			case ".bmp":
-			case ".png":
-			case ".tiff":
-				imageTag = '<img src="' + imageFullURL + '" /> ';
+			case '.jpg':
+			case '.jpeg':
+			case '.gif':
+			case '.bmp':
+			case '.png':
+			case '.tiff':
+				imageTag = '<img src="' + imageFullURL + '"/> ';
 				break;
 
 			default:
-				imageTag = '<a href="' + imageFullURL + '" >' + imageFullURL + '</a> ';
+				imageTag = '<a href="' + imageFullURL + '">' + imageFullURL + '</a> ';
 				break;
 		}
 	}
