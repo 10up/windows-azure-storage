33a47786babe64ae0f9f133f8c15bc462ceb1285
Morgan Estes <morgan.estes@10up.com>
2016-01-23 02:09:43 -0600

    Azure plugin: update the search form's action check
    Lowercase values, single quotes, strict equality, and Yoda Conditions.
    These are the hallmarks of WordPress.

--- windows-azure-storage-dialog.php
+++ windows-azure-storage-dialog.php
@@ -186,7 +186,7 @@ function windows_azure_storage_dialog_browse_tab() {
 		}
 
 		// Handle file search
-		if ( ( ! empty( $_POST['action'] ) ) && ( $_POST["action"] == "Search" ) ) {
+		if ( isset( $_POST['action'] ) && ( 'search_azure_storage' === $_POST['action'] ) ) {
 			try {
 				$fileTagFilter   = sanitize_text_field( $_POST["searchFileTag"] );
 				$fileNameFilter  = sanitize_text_field( $_POST["searchFileName"] );
