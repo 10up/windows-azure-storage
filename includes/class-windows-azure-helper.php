4d693aaf2eb8bec38ec5b36365654bb47775b79c
Eugene Manuilov <eugene.manuilov@10up.com>
2017-11-01 21:47:22 +0200

    Added cache control header to the blob properties

--- includes/class-windows-azure-helper.php
+++ includes/class-windows-azure-helper.php
@@ -392,8 +392,10 @@ class Windows_Azure_Helper {
 		$finfo     = finfo_open( FILEINFO_MIME_TYPE );
 		$mime_type = finfo_file( $finfo, $local_path );
 		finfo_close( $finfo );
+		
 		$rest_api_client->put_blob_properties( $container_name, $remote_path, array(
 			Windows_Azure_Rest_Api_Client::API_HEADER_MS_BLOB_CONTENT_TYPE => $mime_type,
+			Windows_Azure_Rest_Api_Client::API_HEADER_CACHE_CONTROL        => sprintf( "max-age=%d, must-revalidate", Windows_Azure_Helper::get_cache_control() ),
 		) );
 
 		return $result;
@@ -460,6 +462,7 @@ class Windows_Azure_Helper {
 
 		$rest_api_client->put_blob_properties( $container_name, $blob_name, array(
 			Windows_Azure_Rest_Api_Client::API_HEADER_MS_BLOB_CONTENT_TYPE => $mime_type,
+			Windows_Azure_Rest_Api_Client::API_HEADER_CACHE_CONTROL        => sprintf( "max-age=%d, must-revalidate", Windows_Azure_Helper::get_cache_control() ),
 		) );
 
 		return $result;
